e629b121eb7bf439cc9a4373c6de411ef98e3bf1
vaishnavix <107755052+vaishnavix@users.noreply.github.com>
2022-06-24 15:28:47 +0400

    Add files via upload

---
+++ Project-Template-C9--main/sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red")
+    
+    
+  }
+  else{
+background("yellow")
+  }
+ 
+  
+  drawSprites();
+}
+
